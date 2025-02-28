78ea47e497982b0abdf96c3d265ea07a9610c097
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2025-02-28 08:02:09 +0100

    Adding functionality to get all user ratings

--- RestApiCourse/Movies.Application/Repositories/RatingRepository.cs
+++ RestApiCourse/Movies.Application/Repositories/RatingRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using Movies.Application.Database;
+using Movies.Application.Models;
 
 namespace Movies.Application.Repositories;
 
@@ -62,5 +63,17 @@ public class RatingRepository : IRatingRepository
             """, new { movieId, userId }, cancellationToken: token));
         
         return result > 0;
+    }
+
+    public async Task<IEnumerable<MovieRating>> GetRatingsForUserAsync(Guid userId, CancellationToken token = default)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+        
+        return await connection.QueryAsync<MovieRating>(new CommandDefinition("""
+            select r.rating, r.movieid, m.slug
+            from ratings r
+            inner join movies m on r.movieid = m.id
+            where r.userid = @userId
+            """, new { userId }, cancellationToken: token));
     }
 }
