60347342f3d54a774c7c736cac2aee7803be9c08
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2025-02-28 17:45:59 +0100

    Integrating versioning in Swagger

--- RestApiCourse/Movies.Api/Program.cs
+++ RestApiCourse/Movies.Api/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.Text;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Movies.Api.Auth;
 using Movies.Api.Mapping;
+using Movies.Api.Swagger;
 using Movies.Application;
 using Movies.Application.Database;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -49,12 +52,14 @@ builder.Services.AddApiVersioning(x =>
     x.ReportApiVersions = true;
     // x.ApiVersionReader = new HeaderApiVersionReader("api-version");
     x.ApiVersionReader = new MediaTypeApiVersionReader("api-version");
-}).AddMvc();
+}).AddMvc().AddApiExplorer();
 
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => x.OperationFilter<SwaggerDefaultValues>());
 
 
 //builder.Services.AddSingleton<IMovieRepository, MovieRepository>();
@@ -67,7 +72,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x =>
+    {
+        foreach (var description in app.DescribeApiVersions())
+        {
+            x.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName);
+        }
+    });
 }
 
 app.UseHttpsRedirection();
