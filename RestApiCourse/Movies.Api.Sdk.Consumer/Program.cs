6afb5cea2145e754fdcfb14fd5243f7d3c5a6fac
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2025-03-01 17:02:48 +0100

    Adding SDK and Client for testing it using Refit

---
+++ RestApiCourse/Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+// var movie = await moviesApi.GetMovieAsync("nick-the-greek-2023");
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+};
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
---
+++ RestApiCourse/Movies.Api.Sdk/ApiEndpoints.cs
@@ -0,0 +1,29 @@
+﻿namespace Movies.Api.Sdk;
+
+// Note: This file is copied from Movies.Api
+public static class ApiEndpoints
+{
+    private const string ApiBase = "/api"; // Note: Slash has beed added in fron of 'api'
+
+    public static class Movies
+    {
+        private const string Base = $"{ApiBase}/movies";
+
+        public const string Create = Base;
+        //public const string Get = $"{Base}/{{id}}";
+        public const string Get = $"{Base}/{{idOrSlug}}";
+        public const string GetAll = Base;
+        public const string Update = $"{Base}/{{id}}";// Note: ':guid' constraint has been removed
+        public const string Delete = $"{Base}/{{id}}";
+
+        public const string Rate = $"{Base}/{{id}}/ratings";
+        public const string DeleteRating = $"{Base}/{{id}}/ratings";
+    }
+
+    public static class Ratings
+    {
+        private const string Base = $"{ApiBase}/ratings";
+
+        public const string GetUserRatings = $"{Base}/me";
+    }
+}
