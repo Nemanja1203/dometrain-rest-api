db565560d82bbff04f1fe61838f859ddb21787dc
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2025-03-02 07:04:49 +0100

    Updating versioning

--- RestApiCourse/Movies.Api/Endpoints/Movies/GetAllMoviesEndpoint.cs
+++ RestApiCourse/Movies.Api/Endpoints/Movies/GetAllMoviesEndpoint.cs
@@ -32,7 +32,33 @@ public static class GetAllMoviesEndpoint
                 return TypedResults.Ok(moviesResponse);
             })
             .Produces<MoviesResponse>(StatusCodes.Status200OK)
-            .WithName(Name);
+            .WithName($"{Name}V1")
+            .WithApiVersionSet(ApiVersioning.VersionSet)
+            .HasApiVersion(1.0);
+        
+        app.MapGet(ApiEndpoints.Movies.GetAll, async (
+                [AsParameters] GetAllMoviesRequest request,
+                IMovieService movieService,
+                HttpContext context,
+                CancellationToken token) =>
+            {
+                var userId = context.GetUserId();
+                var options = request.MapToOptions()
+                    .WithUser(userId);
+
+                var movies = await movieService.GetAllAsync(options, token);
+                var moviesCount = await movieService.GetCountAsync(options.Title, options.YearOfRelease, token);
+
+                var moviesResponse = movies.MapToResponse(
+                    request.Page.GetValueOrDefault(PagedRequest.DefaultPage), 
+                    request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize), 
+                    moviesCount);
+                return TypedResults.Ok(moviesResponse);
+            })
+            .Produces<MoviesResponse>(StatusCodes.Status200OK)
+            .WithName($"{Name}V2")
+            .WithApiVersionSet(ApiVersioning.VersionSet)
+            .HasApiVersion(2.0);
 
         return app;
     }
